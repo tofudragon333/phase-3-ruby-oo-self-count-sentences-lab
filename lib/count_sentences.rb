5dae4bf09ffa202997527db8ea569d49653f75ea
Ashley Wong <tofudragon333@gmail.com>
2022-06-30 01:34:37 -0400

    commit tree 13cd526dc303e1577f1eadb80f60cbcc2fb372a9

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,39 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".") 
+      true
+    else
+      false
+    end
   end
 
   def question?
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
 
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    sentences = self.split(/\.|\?|!/)
+    actual_sentences = sentences.filter do |sentence|
+      sentence != ""
+    end
+    # binding.pry
+    actual_sentences.count
   end
 end
+
+"hello!!! how are you? I'm good...".count_sentences
+# binding.pry
